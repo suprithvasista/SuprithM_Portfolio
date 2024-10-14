757c0bd48486810e94760a59b85befa5b7b90b1c
suprithvasista <suprithvasista829@gmail.com>
2024-10-15 03:01:30 +0530

    upadted public url

--- lib/widgets/mian_mobile.dart
+++ lib/widgets/mian_mobile.dart
@@ -86,7 +86,7 @@ class MainMobile extends StatelessWidget {
 
   // Function to download PDF from the local files in the web directory
   void downloadPDF() {
-    const url = 'public/suprith_m_resume.pdf'; // The relative path to your PDF in the web directory
+    const url = '/suprith_m_resume.pdf'; // The relative path to your PDF in the web directory
     final anchor = html.AnchorElement(href: url)
       ..setAttribute('download', 'suprith_m_resume.pdf') // Name of the downloaded file
       ..click();
